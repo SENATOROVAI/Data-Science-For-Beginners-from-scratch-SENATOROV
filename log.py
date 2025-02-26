d3aeb87cfae86ddfcc586c618185742b6d9c0852
Maryia Krauchanka <krauchankamaryia@gmail.com>
2025-02-26 12:03:59 +0200

    Commit log

--- log.py
+++ log.py
@@ -1,4 +1,21 @@
 """Lessons log."""
 
-# ## 04.02.2025
+# #### 04.02.2025
 # Repository initialization, first commit and push
+
+# #### 26.02.2025
+# - Data processing workflow
+# - Understanding Linear Regression
+#   - One dimensional linear regression
+#   - Best fit line
+#   - Derivative definition
+#   - Norms
+#     - L1 norm, also known as the Manhattan norm
+#     - L2 norm, also known as the Euclidean norm
+#   - Smooth vs. Non-Smooth Functions
+#   - Functional Loss
+#   - Numerical method(iterations) in finding coefficients
+#   - Definition of predictor(feature, independent) and target(dependent) variables
+#   - Hypothesis function
+#     - Liniarity
+#     - Independence
