add0fadc07898d1fba2ae0de9391fa7c3a1bc6be
Maryia Krauchanka <krauchankamaryia@gmail.com>
2025-03-06 09:37:41 +0200

    commit log

--- log.py
+++ log.py
@@ -19,3 +19,83 @@
 #   - Hypothesis function
 #     - Liniarity
 #     - Independence
+
+# ### 05.03.2025
+#
+# - Road map for Linear Regression
+#   - Introduction
+#     - Definition
+#     - Types of Linear Regressions
+#       - Simple
+#       - Multiple
+#   - Basic definitions
+#     - Variables
+#       - target - dependent variable
+#       - feature - independent variable
+#     - Linear Regression Equation
+#       - Simple
+#       - Multiple
+#     - Hypothesis
+#   - Estimation of model parameters
+#     - Ordinary Least Squares
+#     - Gradient Descent
+#   - Model quality estimation
+#     - Metrics
+#       - MAE (Mean Absolute Error)
+#       - MSE (Mean Squared Error)
+#       - RMSE (Root Mean Squared Error)
+#       - R² (Коэффициент детерминации)
+#   - Checking the Assumptions of Linear Regression
+#     - Linearity of the relationship
+#     - Normality of residuals
+#     - Homoscedasticity
+#     - Absence of multicollinearity between independent variables
+#   - Problems and solutions
+#     - Overfitting
+#       - Regularization L1 and L2
+#     - Underfitting
+#       -  Add more features and use more complicated model
+#     - Multicollinearity
+#       - Removing correlated variables or using regularization (Ridge, Lasso)
+#   - Regularization
+#     - Ridge (L2)
+#     - Lasso (L1)
+#     - Elastic Net (L1 + L2)
+#   - Advanced
+#     - Multiple Linear Regression — with multiple predictors.
+#     - Polynomial Regression — nonlinear relationships.
+#     - Regularization (Ridge, Lasso, Elastic Net).
+#     - Feature Scaling — StandardScaler, MinMaxScaler.
+#     - Checking Assumptions using Residual Plots and QQ-Plots.
+#
+# - Machine learning
+#   - Supervised
+#     - Regression
+#     - Classification
+#   - Unsupervised
+#     - Clustering
+#
+#
+# - Classification of Regression Models
+#
+#     - Regression
+#         - By Number of Factors
+#             - Simple (Pairwise) Regression
+#             - Multiple Regression
+#         - By Relationship Type
+#             - Linear
+#             - Nonlinear
+#                 - Linear by Variables
+#                 - Linear by Parameters
+#                 - Internally Nonlinear
+#
+#
+# - Nonlinear functions
+#   - Power Function Model – Used for dependencies with constant elasticity.
+#   - Exponential Model – Used for processes with a constant growth rate.
+#   - Logarithmic Model – Used for dependencies with a constant decrease in increment.
+#   - Hyperbolic Model – Used for dependencies with a lower or upper limit.
+#   - Polynomial Model of Different Degrees – Used for relationships with changing direction.
+#
+# - Steps for Building a Model
+# - Probabilistic and non-probabilistic or deterministic approaches
