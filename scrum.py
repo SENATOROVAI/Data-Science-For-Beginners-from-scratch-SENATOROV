88df3a2d49c9d59596d908a495cf4088cb281f6d
Maryia Krauchanka <krauchankamaryia@gmail.com>
2025-05-05 16:01:49 +0300

    log scrum

---
+++ scrum.py
@@ -0,0 +1,6 @@
+"""Scrum log."""
+
+# #### 05.05.2025
+#
+# - In progress with issue https://github.com/SENATOROVAI/DA/issues/7
+# - Currently on step devoted to practice EDA - https://colab.research.google.com/drive/12eJv82hBfFospahDbnB6tNjwRCkCmUqP?usp=sharing
