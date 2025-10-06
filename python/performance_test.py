51b650ec1b018d432f5a2ef384e034fe45c31519
Quantum117 <iskandarburiev42@gmail.com>
2025-10-07 01:15:51 +0500

    Cpython  #4 (https://github.com/SENATOROVAI/intro-cs/issues/4 )
    closes https://github.com/SENATOROVAI/intro-cs/issues/4

---
+++ python/performance_test.py
@@ -0,0 +1,13 @@
+"""Performance test script to compare execution time of a simple loop in
+Python."""
+
+import time
+
+start_time = time.time()
+total = 0
+for i in range(1, 10000000):
+    total += i
+end_time = time.time()
+
+print("Result:", total)
+print("Execution time:", end_time - start_time, "seconds")
