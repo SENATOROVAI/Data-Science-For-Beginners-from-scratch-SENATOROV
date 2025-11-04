a9efb785da6317f9abe70eecfb788adc0729182f
Quantum117 <iskandarburiev42@gmail.com>
2025-11-05 00:42:21 +0500

    Issue #7 (https://github.com/SENATOROVAI/intro-cs/issues/7 )
    closes https://github.com/SENATOROVAI/intro-cs/issues/7

--- log.py
+++ log.py
@@ -1,1 +1,25 @@
 """Module for reports ."""
+
+# virtual environment
+# get dependencies -> pip freeze > requirements.txt
+# create virtual env -> python -m venv venv (name of a folder)
+# activate venv -> venv\Scripts\activate
+# To install dependencies -> pip install -r requirements.txt
+#
+
+# 📦 1. Package
+# A package is just a collection of Python code that someone wrote so you can reuse it.
+#
+# 🧱 2. Environment
+# An environment is a separate space where you can install packages.
+#
+# 🔗 3. Dependency
+# A dependency is a package that another package needs to work.
+
+# To upload dependencies :
+# conda env export > environment.yml
+#
+# To install dependencies :
+# conda env create -f environment.yml
+
+# ! - восклицательный знак позволяет использовать ячейку как терминал
