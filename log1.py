9f100e3117a44f8904cee7495e91841a72961ef6
Svetlana Sabitova <svetamath88@gmail.com>
2026-01-08 04:03:27 +0500

    [TASK] Quiz #6 (https://github.com/SENATOROVAI/intro-cs/issues/6)
    Closes https://github.com/SENATOROVAI/intro-cs/issues/6

--- log1.py
+++
@@ -1,9 +0,0 @@
-"""Module for logging experiment results.
-
-This notebook contains utilities for logging and analysis.
-"""
-
-# 04/01
-# 1. Закончила выполнение intro, c Cursor разобралась
-# 2. Начала проходить курс по Python
-#
