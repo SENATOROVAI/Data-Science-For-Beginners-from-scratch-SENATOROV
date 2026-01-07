67ca237501f90bbdd056e9855c6bbfa3a649d700
Pavel Demyanov <stokato90@gmail.com>
2026-01-07 04:06:17 +0300

    [TASK] Quiz  #6 (https://github.com/SENATOROVAI/intro-cs/issues/6)
    Closes https://github.com/SENATOROVAI/intro-cs/issues/6

---
+++ log.py
@@ -0,0 +1,15 @@
+"""Лог уроков."""
+
+# 30/12
+#
+# 1. Продолжаю интро из Сенаторов АИ на 7 пункте
+#
+
+# 07/01
+#
+# 1. Доделаны все квизы
+# 2. Пересмотрены по несколько раз видео:
+#    https://www.youtube.com/watch?v=Si9MfV8uJ-0
+#    https://www.youtube.com/watch?si=tCEVs00xGt2q61eW&v=hW_7hodGxVU&feature=youtu.be
+# 3. Изучаю курс по пайтону шаг https://stepik.org/lesson/1711495/step/7?unit=1734919
+#
