9408c74795da7f41b1f158324cd8c612119d20e4
Pavel Demyanov <stokato90@gmail.com>
2026-01-08 15:02:08 +0300

    [TASK] Cpython  #4 (https://github.com/SENATOROVAI/intro-cs/issues/4)
    Closes https://github.com/SENATOROVAI/intro-cs/issues/4

--- log.py
+++ log.py
@@ -12,4 +12,6 @@
 #    https://www.youtube.com/watch?v=Si9MfV8uJ-0
 #    https://www.youtube.com/watch?si=tCEVs00xGt2q61eW&v=hW_7hodGxVU&feature=youtu.be
 # 3. Изучаю курс по пайтону шаг https://stepik.org/lesson/1711495/step/7?unit=1734919
+# 4. https://github.com/SENATOROVAI/intro-cs/issues/6 - сделал пулл
+# 5. https://github.com/SENATOROVAI/intro-cs/issues/4 - сделал пулл
 #
