b5ee2b1fcc8691a4939e4f07ebde2e23f7eae818
Toleranzy <cergand07@mail.ru>
2026-07-28 23:36:13 +0300

    First_point

---
+++ python/makarov/variables_in_python_1.py
@@ -0,0 +1,46 @@
+"""Variables in python."""
+
+X1 = 15
+print(X1)
+
+Y1 = "Я прогаю на питоне"
+print(Y1)
+
+A2, B2, C2 = "Питон", "C++", "PHP"
+print(A2, B2, C2)
+
+X3 = Y3 = Z3 = "То самое значение"
+print(X3, Y3, Z3)
+
+MY_LIST = ["помидоры", "огурцы", "картофель"]
+A4, B4, C4 = MY_LIST
+print(A4, B4, C4)
+
+# +
+INTEGER_VALUE = 256
+FLOAT_VALUE = 0.25
+TEXT_VALUE = "Просто текст"
+
+print("INTEGER_VALUE:", "FLOAT_VALUE:", "TEXT_VALUE:")
+# -
+
+XXX_5 = str(25)
+YYY_5 = int(25)
+ZZZ_5 = float(25)
+print(type(XXX_5), type(YYY_5), type(ZZZ_5))
+
+print(type(int("25")))
+
+print(type(float("25.5")))
+
+print(int(36.6))
+print(type(int(36.6)))
+
+print(type(str(25)))
+print(type(str(36.6)))
+
+VARIABLE = "переменная"
+_VARIABLE = "переменная"
+VARIABLE_ = "переменная"
+MY_VARIABLE = "переменная"
+MY_VARIABLE_123 = "переменная"
